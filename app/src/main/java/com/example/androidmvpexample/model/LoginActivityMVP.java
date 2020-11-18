138bb132774eef90c48fb36d7499204fa9351080
anastasiaShpakava <shpakavaa@gmail.com>
2020-11-18 13:11:25 +0300

    added model class and interface for using MVP design pattern,

--- app/src/main/java/com/example/androidmvpexample/model/LoginActivityMVP.java
+++ app/src/main/java/com/example/androidmvpexample/model/LoginActivityMVP.java
@@ -1,4 +1,23 @@
 package com.example.androidmvpexample.model;
 
 public interface LoginActivityMVP {
+
+    interface View{
+        String getFirstName();
+        String getLastName();
+
+        void showInputError();
+        void setFirstName(String firstName);
+        void setLastName(String lastName);
+        void showUserSavedMessage();
+    }
+    interface Presenter {
+        void setView(View view);
+        void loginButtonClicked();
+        void getCurrentUser();
+    }
+    interface Model {
+        void createUser(String name, String lastName);
+        User getUser();
+    }
 }
